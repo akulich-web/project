ba68c03c0700c2cca4fbd460cf6aef0d9ac39bf7
Alesia Akulich <alesia.akulich90@mail.ru>
2021-01-31 17:55:37 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 var backButton = document.querySelector('.arrow-left');
 var nextButton = document.querySelector('.arrow-right');
 
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: backButton,
+    nextArrow: nextButton,
+    responsive: [
+        {
+            breakpoint: 768,
+                settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]      
+}); 
