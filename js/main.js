54014f04d6f25a1e3e1abf0acc335feba68e1078
Alesia Akulich <alesia.akulich90@mail.ru>
2021-03-03 10:37:16 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -92,23 +92,7 @@ $(document).ready(function() {
 
 $(function() {
 
-$("[data-scroll").on("click", function(event) {
-    event.preventDefault();
 
-    
-
-    var $this = $(this),
-        blockId = $(this).data('scroll'),
-        blockOffset = $(blockId).offset().top;
-        
-        
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
-
-        $("html, body").animate({
-            scrollTop: blockOffset 
-        }, 500);
-});
 
 
 $("#nav-toggle").on("click", function(event) {
